f6b6eac7bc9c130dc691072f73efa301b4ee3ca3
Shreyasb1015 <bagweshreyas1015@gmail.com>
2023-11-11 19:58:24 +0530

    int and float concept

---
+++ int-and-floats/intfloat.py
@@ -0,0 +1,43 @@
+
+num=3
+# We can check datatype of variable by type() i.e inbulit.
+print(type(num))
+num=6.70
+print(type(num))
+
+#Arithmetic Operators
+#Addition => 3+4
+#Subtraction =>4-3
+#Multiplication =>3*4
+#Division => 12/4
+#Floor Division => 3//2
+#Modulus => 3%2
+#Exponent => 3**2
+
+print(3//2)
+print(3**2)
+
+#increment =>
+num += 5
+print(num)
+
+#To print the absolute value of number we use abs(), which is inbulit=>
+print(abs(-5))
+
+#To get the rounding off of number, we use round() ,which is inbuilt =>
+print(round(3.78))
+
+#We can also round off the number upto specified positions after decimal point => For that we pass 2 arguments - number and total no that we want after decimal point
+print(round(3.76,1))
+
+#We use int() to convert the string into integer value
+
+num1='100'
+num2='200'
+
+num1=int(num1)
+num2=int(num2)
+
+print(num1+num2)
+
+
