10eaa02a9dc93a1e2369b2ed863190738e904d30
Shreyasb1015 <bagweshreyas1015@gmail.com>
2023-12-24 15:42:03 +0530

    inheritance

--- oops/inheritance.py
+++ oops/inheritance.py
@@ -1,1 +1,34 @@
 #Inheritance allows you to inherit the properties of a class, i.e., base class to another, i.e., derived class.
+
+
+#Creating a Parent Class
+
+class Person():
+    
+    def __init__(self,name,id):
+        self.name=name
+        self.id=id
+        
+    def display(self):
+        print(self.name,self.id)
+        
+
+
+emp=Person("Shreyas",10)
+emp.display()
+        
+
+#Creating a child class
+
+class Emp(Person):
+    
+    def Print(self):
+        print("Emp class was called !")
+        
+
+Emp_details=Emp("Rohit",45)
+
+Emp_details.display()
+
+Emp_details.Print()
+ 
