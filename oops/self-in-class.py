04e659165aa82226e59e64e603131d0e3ed9a909
Shreyasb1015 <bagweshreyas1015@gmail.com>
2023-12-24 13:10:35 +0530

    self in class

--- oops/self-in-class.py
+++ oops/self-in-class.py
@@ -2,3 +2,16 @@
 # It is customary to use “self” as the first parameter in instance methods of a class. 
 # Whenever you call a method of an object created from a class, the object is automatically passed as the first argument using the “self” parameter. 
 # This enables you to modify the object’s properties and execute tasks unique to that particular instance.
+
+class Number():
+    
+    def __init__(self,value):
+        self.value=value
+        
+    def print_value(self):
+        print(self.value)
+        
+
+obj1=Number(17)
+obj1.print_value()
+        
