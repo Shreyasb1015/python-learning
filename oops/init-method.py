3ea72f956151102301c4903f25d5f449d1c966ee
Shreyasb1015 <bagweshreyas1015@gmail.com>
2023-12-24 13:38:02 +0530

    init method in python

---
+++ oops/init-method.py
@@ -0,0 +1,26 @@
+### __init__ method is similar to constructors in java/C++.It is used to initiliase the object.
+
+class Dog:
+ 
+    # class attribute
+    attr1 = "mammal"
+ 
+    # Instance attribute
+    def __init__(self, name):
+        self.name = name
+ 
+# Driver code
+# Object instantiation
+Rodger = Dog("Rodger")
+Tommy = Dog("Tommy")
+ 
+# Accessing class attributes
+print("Rodger is a {}".format(Rodger.__class__.attr1))
+print("Tommy is also a {}".format(Tommy.__class__.attr1))
+ 
+# Accessing instance attributes
+print("My name is {}".format(Rodger.name))
+print("My name is {}".format(Tommy.name))
+
+
+### Note : Class attributes are shared by all objects.
