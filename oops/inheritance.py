08412fd2ced75097480aae4729ed3bc3b6cb3d79
Shreyasb1015 <bagweshreyas1015@gmail.com>
2023-12-24 16:37:27 +0530

    inheritance

--- oops/inheritance.py
+++ oops/inheritance.py
@@ -158,4 +158,50 @@ c.printStrs()
 #Multilevel Inheritance  
 
 
+class Base(object):
+ 
+    # Constructor
+    def __init__(self, name):
+        self.name = name
+ 
+    # To get name
+    def getName(self):
+        return self.name
+ 
+ 
+# Inherited or Sub class (Note Person in bracket)
+class Child(Base):
+ 
+    # Constructor
+    def __init__(self, name, age):
+        Base.__init__(self, name)
+        self.age = age
+ 
+    # To get name
+    def getAge(self):
+        return self.age
+ 
+# Inherited or Sub class (Note Person in bracket)
+ 
+ 
+class GrandChild(Child):
+ 
+    # Constructor
+    def __init__(self, name, age, address):
+        Child.__init__(self, name, age)
+        self.address = address
+ 
+    # To get address
+    def getAddress(self):
+        return self.address
+ 
+ 
+# Driver code
+g = GrandChild("Shreyas", 23, "Noida")
+print(g.getName(), g.getAge(), g.getAddress())
+
+###We don’t always want the instance variables of the parent class to be inherited by the child class .
+# we can make some of the instance variables of the parent class private, which won’t be available to the child class. 
+
+#In Python inheritance, we can make an instance variable private by adding double underscores before its name.
 
