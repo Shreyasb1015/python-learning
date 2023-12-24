01ad0c4788fa0010138d172ec85c5ab1f9b28641
Shreyasb1015 <bagweshreyas1015@gmail.com>
2023-12-24 13:07:49 +0530

    oops concept

---
+++ oops/self-in-class.py
@@ -0,0 +1,4 @@
+#When working with classes in Python, the term “self” refers to the instance of the class that is currently being used. 
+# It is customary to use “self” as the first parameter in instance methods of a class. 
+# Whenever you call a method of an object created from a class, the object is automatically passed as the first argument using the “self” parameter. 
+# This enables you to modify the object’s properties and execute tasks unique to that particular instance.
