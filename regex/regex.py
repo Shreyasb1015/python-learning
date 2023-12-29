afc4788c0cc3909dfd7fc42a1e75be291d6df081
Shreyasb1015 <bagweshreyas1015@gmail.com>
2023-12-29 13:11:16 +0530

    regex

--- regex/regex.py
+++ regex/regex.py
@@ -1,3 +1,35 @@
 
 #Importing necessary module
 import re
+
+text_to_search = '''
+abcdefghijklmnopqurtuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+1234567890
+
+Ha HaHa
+
+MetaCharacters (Need to be escaped):
+. ^ $ * + ? { } [ ] \ | ( )
+
+coreyms.com
+
+321-555-4321
+123.555.1234
+123*555*1234
+800-555-1234
+900-555-1234
+
+Mr. Schafer
+Mr Smith
+Ms Davis
+Mrs. Robinson
+Mr. T
+'''
+
+sentence = 'Start a sentence and then bring it to an end'
+
+##### We will be using raw strings so that python doesnt interfare with our code.
+#### For e.g if we print('\tTab')  -->output -->  Tab
+#### Now ,if we use raw strings , we will do  --> print(r'\tTab') -> Output \tTab
+
