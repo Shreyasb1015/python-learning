c11ce72f15c8de586b734b7bea33dafd4a78c5ad
Shreyasb1015 <bagweshreyas1015@gmail.com>
2023-12-30 15:28:03 +0530

    file operations

--- file-objects/demo.py
+++ file-objects/demo.py
@@ -3,3 +3,18 @@
 ## The method below is not recommended, because it is easy to forget to close the file.
 
 f = open('test.txt', 'r')
+#If we want to read and write the file , we can specify it using r+ or w+.
+
+print(f.name)
+
+#To check the mode of file , we can do -->
+print(f.mode)
+
+
+f.close()
+
+#Recommended way to open a file is using the with statement.
+
+with open('test.txt','r') as f:
+    print(f.name)
+    
