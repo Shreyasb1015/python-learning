554306140583694f9ca2dfd4f18ba3743b732f10
Shreyasb1015 <bagweshreyas1015@gmail.com>
2024-01-15 19:36:40 +0530

    numpy revision

--- libraries/numpy/revise.py
+++ libraries/numpy/revise.py
@@ -4,3 +4,10 @@ import numpy as np
 
 arr=np.array([1,2,3,4,5,6,7,8,9,10])
 print(arr)
+
+arr2=np.array([1,2,3,4,5,6],dtype='i')
+print(arr2)
+
+#Creating an array full of zeros
+arr3=np.zeros((3,4))
+print(arr3)
