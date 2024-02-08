c2221140f4ffa3dd390f535f7180e9f01966f131
Shreyasb1015 <bagweshreyas1015@gmail.com>
2024-02-08 16:59:14 +0530

    updated files

---
+++ data-structures/decimaltoBinary.py
@@ -0,0 +1,13 @@
+from stack import Stack
+
+s=Stack()
+
+num=int(input("Enter any number: "))
+
+while(num !=0):
+    s.push(num % 2)
+    num=num // 2
+
+
+print("Binary representation:")
+s.reversestack()
--- data-structures/stack.py
+++ data-structures/stack.py
@@ -1,33 +1,35 @@
-l1=[]
-l1.append(1)
-l1.append(2)
-l1.append(3)
-l1.append(4)
-print(l1)
-choice=0
-while choice <4:
-    print("Stack operations")
-    print("1.push")
-    print("2.pop")
-    print("3.Search")
-    print("4.exit")
-    choice=int(input("Enter your choice: "))
-    if choice==1:
-        ele=int(input("Enter the element: "))
-        l1.append(ele)
-        print(l1)
-    elif choice==2:
+class Stack():
+
+    def _init_(self):
+        self.s=[]
+    
+    def push(self,ele):
+        self.s.append(ele)
+    
+    def pop(self):
         try:
-            l1.pop()
-            print(l1)
-        except IndexError:
-            print("Stack is empty")
-    elif choice==3:
+            self.s.pop()
+            print(self.s)
+        except  IndexError:
+            print("Stack has no element to pop")
+    
+    def search(self):
         element=int(input("enter element "))
         try:
-            pos=l1.index(element)
+            pos=self.s.index(element)
             print(f"element found at position {pos} ")
         except ValueError:
             print('element not found')
-    elif choice==4:
-        break
+    
+    def peek(self):
+        try:
+          print(self.s[-1])
+        except ValueError:
+            print('Stack is empty')
+    
+    def reversestack(self):
+       reversed_stack=list(reversed(self.s))
+       print(reversed_stack)
+
+    def printstack(self):
+            print(self.s)
